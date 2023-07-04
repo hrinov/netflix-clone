0a1b685a43603d98f04283c4aa2b9ca3e71147f4
Ruslan Hrinov <121380401+ruslan-hrinov@users.noreply.github.com>
2023-07-04 11:32:01 +0300

    commit

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+function Footer({}): JSX.Element {
+  return (
+    <footer className="p-4 text-center">
+      <p className="text-2xl">Made by Ruslan Hrinov</p>{' '}
+    </footer>
+  );
+}
+export default Footer;
